8e383407581692479c72f10fa34b7b507b7be3b8
Marc Pouliquen <marc.pouliquen@inria.fr>
2017-07-06 15:38:38 +0200

    blob tracker corrected, doing a copy of the frame in the plugin and using a try catch to prevent failing tracking

--- vispWrapper/visp-demo.cpp
+++ vispWrapper/visp-demo.cpp
@@ -1,60 +1,65 @@
 #include "visp-demo.h"
 extern "C" {
 
-	// Declaring VpImage globally
-	vpImage<unsigned char> image;
+// Declaring VpImage globally
+vpImage<unsigned char> image;
 
-	// Declaring blob
-	vpDot2 blob;
+// Declaring blob
+vpDot2 blob;
 
-	// Declaring
-	vpImagePoint germ;
+// Declaring
+vpImagePoint germ;
 
-	// Declaring
-	vpImagePoint cog;
-	double dot_prod(unsigned int* const A){
-		//Defining (1 X 3) Row Vector
-		vpRowVector r(3);
-		r[0] = A[0];
-		r[1] = A[1];
-		r[2] = A[2];
-		//Defining (3 X 1) Coulmn Vector
-		vpColVector c(3);
-		c[0] = A[3];
-		c[1] = A[4];
-		c[2] = A[5];
+// Declaring
+vpImagePoint cog;
+double dot_prod(unsigned int* const A){
+  //Defining (1 X 3) Row Vector
+  vpRowVector r(3);
+  r[0] = A[0];
+  r[1] = A[1];
+  r[2] = A[2];
+  //Defining (3 X 1) Coulmn Vector
+  vpColVector c(3);
+  c[0] = A[3];
+  c[1] = A[4];
+  c[2] = A[5];
 
-		A[5] = 991;
-		//Dot Product
-		double product = r * c;
-		return product;
-	}
+  A[5] = 991;
+  //Dot Product
+  double product = r * c;
+  return product;
+}
 
-	// Greyscale bitmap from Unity to vpImage
-	void passFrame(unsigned char* const bitmap, int height, int width){
-		// Resize frame according to Webcam input from Unity
-		image.resize(height,width);
+// Greyscale bitmap from Unity to vpImage
+void passFrame(unsigned char* const bitmap, int height, int width){
+  // Resize frame according to Webcam input from Unity
+  image.resize(height,width);
 
-		// Grey Scale Image to be passed in the tracker pipeline
-		image.bitmap = bitmap;
-	}
+  // Grey Scale Image to be passed in the tracker pipeline
+  memcpy(image.bitmap,bitmap,height*width);
+}
 
-	void initBlobTracker(double getMouseX, double getMouseY, unsigned int* const init_done)
-	{
-		// Define Blob initial tracking pixel as a vpImagePoint
-		germ.set_ij(getMouseX, getMouseY);
+void initBlobTracker(double getMouseX, double getMouseY, unsigned int* const init_done)
+{
+  // Define Blob initial tracking pixel as a vpImagePoint
+  germ.set_ij(getMouseX, getMouseY);
 
-		//Initialize blob pixels
-		blob.initTracking(image, germ);
-		init_done[0] = 1;
-	}
+  //Initialize blob pixels
+  blob.initTracking(image, germ);
+  init_done[0] = 1;
+}
 
-	void getBlobCoordinates(double* cogX, double* cogY)
-	{
-		blob.track(image);
-		//vpImagePoint cog = blob.getCog();
-		//cogX[0] = cog.get_i();
-		//cogY[0] = cog.get_j();
-		//return;
-	}
+void getBlobCoordinates(double* cogX, double* cogY, unsigned int* const init_done)
+{
+  try {
+    blob.track(image);
+    vpImagePoint cog = blob.getCog();
+    cogX[0] = cog.get_i();
+    cogY[0] = cog.get_j();
+  }
+  catch(...) {
+    *init_done = 0;
+  }
+}
+
 }
