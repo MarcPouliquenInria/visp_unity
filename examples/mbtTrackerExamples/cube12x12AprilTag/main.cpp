aae77d7553fd0a714d54a944d113ab43f6a4999b
Marc Pouliquen <marc.pouliquen@inria.fr>
2018-06-07 17:13:33 +0200

    added automatic generation of cube.cao given cube edge size

--- examples/mbtTrackerExamples/cube12x12AprilTag/main.cpp
+++ examples/mbtTrackerExamples/cube12x12AprilTag/main.cpp
@@ -1,7 +1,7 @@
-//! \example tutorial-apriltag-detector-live.cpp
-//! [Include]
-#include <visp3/detection/vpDetectorAprilTag.h>
-//! [Include]
+#include <fstream>
+#include <ios>
+#include <iostream>
+
 #include <visp3/gui/vpDisplayGDI.h>
 #include <visp3/gui/vpDisplayOpenCV.h>
 #include <visp3/gui/vpDisplayX.h>
@@ -11,65 +11,103 @@
 #ifdef VISP_HAVE_V4L2
 #include <visp3/sensor/vpV4l2Grabber.h>
 #endif
+#include <visp3/core/vpIoTools.h>
+#include <visp3/core/vpUniRand.h>
+#include <visp3/detection/vpDetectorAprilTag.h>
 #include <visp3/io/vpImageIo.h>
 #include <visp3/mbt/vpMbGenericTracker.h>
+#include <visp3/vision/vpCalibration.h>
 
-vpHomogeneousMatrix detectAprilTag(vpDetectorAprilTag::vpAprilTagFamily tagFamily,
-		    float quad_decimate,
-		    vpDetectorAprilTag::vpPoseEstimationMethod poseEstimationMethod,
-		    int nThreads,
+// creates a cube.cao file in your current directory
+// cubeEdgeSize : size of cube edges in meters
+void createCaoFile(double cubeEdgeSize)
+{
+  std::ofstream fileStream;
+  fileStream.open("cube.cao", std::ofstream::out | std::ofstream::trunc);
+  fileStream << "V1\n";
+  fileStream << "# 3D Points\n";
+  fileStream << "8                  # Number of points\n";
+  fileStream << cubeEdgeSize / 2 << "    " << cubeEdgeSize / 2 << "    " << 0 << "    # Point 0: (X, Y, Z)\n";
+  fileStream << cubeEdgeSize / 2 << "    " << -cubeEdgeSize / 2 << "    " << 0 << "    # Point 1\n";
+  fileStream << -cubeEdgeSize / 2 << "    " << -cubeEdgeSize / 2 << "    " << 0 << "    # Point 2\n";
+  fileStream << -cubeEdgeSize / 2 << "    " << cubeEdgeSize / 2 << "    " << 0 << "    # Point 3\n";
+  fileStream << -cubeEdgeSize / 2 << "    " << cubeEdgeSize / 2 << "    " << -cubeEdgeSize << "    # Point 4\n";
+  fileStream << -cubeEdgeSize / 2 << "    " << -cubeEdgeSize / 2 << "    " << -cubeEdgeSize << "    # Point 5\n";
+  fileStream << cubeEdgeSize / 2 << "    " << -cubeEdgeSize / 2 << "    " << -cubeEdgeSize << "    # Point 6\n";
+  fileStream << cubeEdgeSize / 2 << "    " << cubeEdgeSize / 2 << "    " << -cubeEdgeSize << "    # Point 7\n";
+  fileStream << "# 3D Lines\n";
+  fileStream << "0                  # Number of lines\n";
+  fileStream << "# Faces from 3D lines\n";
+  fileStream << "0                  # Number of faces\n";
+  fileStream << "# Faces from 3D points\n";
+  fileStream << "6                  # Number of faces\n";
+  fileStream << "4 0 3 2 1          # Face 0: [number of points] [index of the 3D points]...\n";
+  fileStream << "4 1 2 5 6\n";
+  fileStream << "4 4 7 6 5\n";
+  fileStream << "4 0 7 4 3\n";
+  fileStream << "4 5 2 3 4\n";
+  fileStream << "4 0 1 6 7          # Face 5\n";
+  fileStream << "# 3D cylinders\n";
+  fileStream << "0                  # Number of cylinders\n";
+  fileStream << "# 3D circles\n";
+  fileStream << "0                  # Number of circles\n";
+  fileStream.close();
+}
+
+vpHomogeneousMatrix detectAprilTag(vpDetectorAprilTag::vpAprilTagFamily tagFamily, float quad_decimate,
+                                   vpDetectorAprilTag::vpPoseEstimationMethod poseEstimationMethod, int nThreads,
 #if defined(VISP_HAVE_V4L2)
-                    vpV4l2Grabber &g,
+                                   vpV4l2Grabber &g,
 #elif defined(VISP_HAVE_OPENCV)
-                    cv::VideoCapture &cap,
+                                   cv::VideoCapture &cap,
 #endif
-                    vpImage<unsigned char> &I,
-		    double tagSize,
-		    vpCameraParameters cam) {
+                                   vpImage<unsigned char> &I, double tagSize, vpCameraParameters cam)
+{
 
-    //Define aprilTag options
-    vpDetectorAprilTag detector(tagFamily);
-    detector.setAprilTagQuadDecimate(quad_decimate);
-    detector.setAprilTagPoseEstimationMethod(poseEstimationMethod);
-    detector.setAprilTagNbThreads(nThreads);
+  // Define aprilTag options
+  vpDetectorAprilTag detector(tagFamily);
+  detector.setAprilTagQuadDecimate(quad_decimate);
+  detector.setAprilTagPoseEstimationMethod(poseEstimationMethod);
+  detector.setAprilTagNbThreads(nThreads);
 
-    std::vector<vpHomogeneousMatrix> cMo_vec;
+  std::vector<vpHomogeneousMatrix> cMo_vec;
+  cv::Mat frame;
 
-    bool runTagDetection = true;
-    while(runTagDetection) {
+  bool runTagDetection = true;
+  while (runTagDetection) {
 #if defined(VISP_HAVE_V4L2)
-      g.acquire(I);
+    g.acquire(I);
 #elif defined(VISP_HAVE_OPENCV)
-      cap >> frame;
-      vpImageConvert::convert(frame, I);
+    cap >> frame;
+    vpImageConvert::convert(frame, I);
 #endif
 
-      vpDisplay::display(I);
+    vpDisplay::display(I);
 
-      // detection
-      detector.detect(I, tagSize, cam, cMo_vec);
+    // detection
+    detector.detect(I, tagSize, cam, cMo_vec);
 
-
-      //Display camera pose
-      for (size_t i = 0; i < cMo_vec.size(); i++) {
-        vpDisplay::displayFrame(I, cMo_vec[i], cam, tagSize / 2, vpColor::none, 3);
-      }
-
-      vpDisplay::displayText(I, 20, 20, "Waiting tag detection.", vpColor::red);
-      vpDisplay::flush(I);
-
-      if(detector.getNbObjects() > 0) { // if tag detected, we pick the first one
-        runTagDetection = false;
-      }
+    // Display camera pose
+    for (size_t i = 0; i < cMo_vec.size(); i++) {
+      vpDisplay::displayFrame(I, cMo_vec[i], cam, tagSize / 2, vpColor::none, 3);
     }
-    std::cout << "AprilTag detected, camera to aprilTag transform :\n" << cMo_vec.at(0) << std::endl;
-    return cMo_vec.at(0);
+
+    vpDisplay::displayText(I, 20, 20, "Waiting tag detection.", vpColor::red);
+    vpDisplay::flush(I);
+
+    if (detector.getNbObjects() > 0) { // if tag detected, we pick the first one
+      runTagDetection = false;
+    }
+  }
+  std::cout << "AprilTag detected, camera to aprilTag transform :\n" << cMo_vec.at(0) << std::endl;
+  return cMo_vec.at(0);
 }
 
 int main(int argc, const char **argv)
 {
 //! [Macro defined]
-#if defined(VISP_HAVE_APRILTAG) && (defined(VISP_HAVE_V4L2) || defined(VISP_HAVE_OPENCV)) && defined(VISP_HAVE_MODULE_MBT)
+#if defined(VISP_HAVE_APRILTAG) && (defined(VISP_HAVE_V4L2) || defined(VISP_HAVE_OPENCV)) &&                           \
+    defined(VISP_HAVE_MODULE_MBT)
   //! [Macro defined]
 
   int opt_device = 0;
@@ -83,6 +121,8 @@ int main(int argc, const char **argv)
   bool display_tag = false;
   int color_id = -1;
   unsigned int thickness = 2;
+  double cubeEdgeSize = 0.125; // 12.5cm by default
+  bool useTexture = false;
 
 #if !(defined(VISP_HAVE_X11) || defined(VISP_HAVE_GDI) || defined(VISP_HAVE_OPENCV))
   bool display_off = true;
@@ -110,42 +150,48 @@ int main(int argc, const char **argv)
     } else if (std::string(argv[i]) == "--display_off") {
       display_off = true;
     } else if (std::string(argv[i]) == "--color" && i + 1 < argc) {
-      color_id = atoi(argv[i+1]);
+      color_id = atoi(argv[i + 1]);
     } else if (std::string(argv[i]) == "--thickness" && i + 1 < argc) {
-      thickness = (unsigned int) atoi(argv[i+1]);
+      thickness = (unsigned int)atoi(argv[i + 1]);
     } else if (std::string(argv[i]) == "--tag_family" && i + 1 < argc) {
       tagFamily = (vpDetectorAprilTag::vpAprilTagFamily)atoi(argv[i + 1]);
+    } else if (std::string(argv[i]) == "--cube_edge_size" && i + 1 < argc) {
+      cubeEdgeSize = atof(argv[i + 1]);
+    } else if (std::string(argv[i]) == "--texture" && i + 1 < argc) {
+      useTexture = true;
     } else if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h") {
-      std::cout << "Usage: " << argv[0]
-                << " [--input <camera input>] [--tag_size <tag_size in m>]"
-                   " [--quad_decimate <quad_decimate>] [--nthreads <nb>]"
-                   " [--intrinsic <intrinsic file>] [--camera_name <camera name>]"
-                   " [--pose_method <method> (0: HOMOGRAPHY, 1: "
-                   "HOMOGRAPHY_VIRTUAL_VS,"
-                   " 2: DEMENTHON_VIRTUAL_VS, 3: LAGRANGE_VIRTUAL_VS,"
-                   " 4: BEST_RESIDUAL_VIRTUAL_VS)]"
-                   " [--tag_family <family> (0: TAG_36h11, 1: TAG_36h10, 2: "
-                   "TAG_36ARTOOLKIT,"
-                   " 3: TAG_25h9, 4: TAG_25h7, 5: TAG_16h5)]"
-                   " [--display_tag]";
+      std::cout << "Usage: " << argv[0] << " [--input <camera input>] [--tag_size <tag_size in m>]"
+                                           " [--quad_decimate <quad_decimate>] [--nthreads <nb>]"
+                                           " [--intrinsic <intrinsic file>] [--camera_name <camera name>]"
+                                           " [--pose_method <method> (0: HOMOGRAPHY, 1: "
+                                           "HOMOGRAPHY_VIRTUAL_VS,"
+                                           " 2: DEMENTHON_VIRTUAL_VS, 3: LAGRANGE_VIRTUAL_VS,"
+                                           " 4: BEST_RESIDUAL_VIRTUAL_VS)]"
+                                           " [--tag_family <family> (0: TAG_36h11, 1: TAG_36h10, 2: "
+                                           "TAG_36ARTOOLKIT,"
+                                           " 3: TAG_25h9, 4: TAG_25h7, 5: TAG_16h5)]"
+                                           " [--display_tag]";
 #if (defined(VISP_HAVE_X11) || defined(VISP_HAVE_GDI) || defined(VISP_HAVE_OPENCV))
       std::cout << " [--display_off] [--color <color id>] [--thickness <line thickness>]";
 #endif
+      std::cout << " [--cube_edge_size <cube_edge_size in m>]" << std::endl;
+      std::cout << " [--texture]" << std::endl;
       std::cout << " [--help]" << std::endl;
       return EXIT_SUCCESS;
     }
   }
 
+  createCaoFile(cubeEdgeSize);
+
   vpCameraParameters cam;
-  cam.initPersProjWithoutDistortion(615.1674805, 615.1675415, 312.1889954, 243.4373779);
+  bool camIsInit = false;
 #ifdef VISP_HAVE_XML2
   vpXmlParserCamera parser;
-  if (!intrinsic_file.empty() && !camera_name.empty())
+  if (!intrinsic_file.empty() && !camera_name.empty()) {
     parser.parse(cam, intrinsic_file, camera_name, vpCameraParameters::perspectiveProjWithoutDistortion);
+    camIsInit = true;
+  }
 #endif
-  std::cout << "cam:\n" << cam << std::endl;
-  std::cout << "poseEstimationMethod: " << poseEstimationMethod << std::endl;
-  std::cout << "tagFamily: " << tagFamily << std::endl;
 
   try {
     vpImage<unsigned char> I;
@@ -168,10 +214,17 @@ int main(int argc, const char **argv)
     cap >> frame; // get a new frame from camera
     vpImageConvert::convert(frame, I);
 #endif
+    if (!camIsInit) {
+      cam.initPersProjWithoutDistortion(600, 600, I.getWidth() / 2., I.getHeight() / 2.);
+    }
 
-    //Construct display
+    std::cout << "cam:\n" << cam << std::endl;
+    std::cout << "poseEstimationMethod: " << poseEstimationMethod << std::endl;
+    std::cout << "tagFamily: " << tagFamily << std::endl;
+
+    // Construct display
     vpDisplay *d = NULL;
-    if (! display_off) {
+    if (!display_off) {
 #ifdef VISP_HAVE_X11
       d = new vpDisplayX(I);
 #elif defined(VISP_HAVE_GDI)
@@ -182,9 +235,12 @@ int main(int argc, const char **argv)
     }
 
     // Prepare MBT
-    vpMbGenericTracker * tracker = new vpMbGenericTracker;
-    tracker->setTrackerType(vpMbGenericTracker::EDGE_TRACKER);
-    //edges
+    vpMbGenericTracker *tracker = new vpMbGenericTracker;
+    if (useTexture)
+      tracker->setTrackerType(vpMbGenericTracker::EDGE_TRACKER | vpMbGenericTracker::KLT_TRACKER);
+    else
+      tracker->setTrackerType(vpMbGenericTracker::EDGE_TRACKER);
+    // edges
     vpMe me;
     me.setMaskSize(5);
     me.setMaskNumber(180);
@@ -194,54 +250,34 @@ int main(int argc, const char **argv)
     me.setMu2(0.5);
     me.setSampleStep(4);
     tracker->setMovingEdge(me);
-    //camera calibration params
+    // camera calibration params
     tracker->setCameraParameters(cam);
-    //model definition
+    // model definition
     tracker->loadModel("cube.cao");
     tracker->setDisplayFeatures(true);
-    tracker->setGoodMovingEdgesRatioThreshold(0.3);
-
-    //Init model-based tracker
-    vpHomogeneousMatrix cubeMapril; //from aprilTag center to cube origin (top-left corer of aprilTag face)
-    cubeMapril.eye();
-    // frame rotation
-    cubeMapril[0][0] = -1;
-    cubeMapril[1][1] = 0;
-    cubeMapril[2][2] = 0;
-    cubeMapril[1][2] = -1;
-    cubeMapril[2][1] = -1;
-    // origin translation (in cube frame)
-    cubeMapril[0][3] = 0.0625;
-    cubeMapril[1][3] = 0;
-    cubeMapril[2][3] = 0.0625;
-
 
     bool run = true;
     vpHomogeneousMatrix cMapril;
     // wait for a tag detection
-    while(run) {
+    while (run) {
 
-        // first we wait for an aprilTag detection
-    cMapril = detectAprilTag(tagFamily, quad_decimate, poseEstimationMethod, nThreads, 
+      // first we wait for an aprilTag detection
+      cMapril = detectAprilTag(tagFamily, quad_decimate, poseEstimationMethod, nThreads,
 #if defined(VISP_HAVE_V4L2)
-                    g,
+                               g,
 #elif defined(VISP_HAVE_OPENCV)
-                    cap,
+                               cap,
 #endif
-                    I, tagSize, cam);
+                               I, tagSize, cam);
 
+      // then we init & run the mbt
+      // build cube to camera transformation
+      tracker->initFromPose(I, cMapril);
 
-    // then we init & run the mbt
-    // build cube to camera transformation
-    vpPoseVector camPcube;
-    camPcube.buildFrom(cMapril * cubeMapril.inverse());
-
-    tracker->initFromPose(I, camPcube);
-
-    //Track model  
-    vpHomogeneousMatrix cMo;
-    bool trackingActive = true;
-      while(trackingActive) {
+      // Track model
+      vpHomogeneousMatrix cMo;
+      bool trackingActive = true;
+      while (trackingActive) {
 #if defined(VISP_HAVE_V4L2)
         g.acquire(I);
 #elif defined(VISP_HAVE_OPENCV)
@@ -251,18 +287,18 @@ int main(int argc, const char **argv)
 
         vpDisplay::display(I);
 
-        //Track
+        // Track
         tracker->track(I);
         tracker->getPose(cMo);
-      
-        //Compute error 
+
+        // Compute error
         double projectionError = tracker->computeCurrentProjectionError(I, cMo, cam);
-        if(projectionError > 30.0) {
-         trackingActive = false;
+        if (projectionError > 30.0) {
+          trackingActive = false;
           std::cout << "Tracking error too high (" << projectionError << "), exiting MBT.\n";
         }
 
-        //Display
+        // Display
         tracker->getCameraParameters(cam);
         tracker->display(I, cMo, cam, vpColor::red, 2);
         vpDisplay::displayFrame(I, cMo, cam, 0.025, vpColor::none, 3);
@@ -271,14 +307,13 @@ int main(int argc, const char **argv)
         vpDisplay::flush(I);
         if (vpDisplay::getClick(I, false)) { // exit
           run = false;
-	  trackingActive = false;
+          trackingActive = false;
         }
       }
     }
 
-    if (! display_off)
+    if (!display_off)
       delete d;
-
   } catch (const vpException &e) {
     std::cerr << "Catch an exception: " << e.getMessage() << std::endl;
   }
